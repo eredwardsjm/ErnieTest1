a190df0a9a9328821b92335552252d41ce3fce71
Ernest Edwards <47692458+eredwardsjm@users.noreply.github.com>
2021-02-05 22:58:19 -0500

    tired

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CustomerComponent } from "./customer/customer.component";
 import { AccountComponent } from "./Account/Account.component";
+import { pagenotfoundComponent } from "./utility/page-not-found.component";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [AppComponent, CustomerComponent, AccountComponent],
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    AccountComponent,
+    pagenotfoundComponent
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: "Customer", component: CustomerComponent },
-      { path: "Account", component: AccountComponent }
+      { path: "Account", component: AccountComponent },
+      { path: "", redirectTo: "/Customer", pathMatch: "full" },
+      { path: "**", component: pagenotfoundComponent }
     ])
   ],
   providers: [],
---
+++ src/app/utility/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "page-not-found",
+  templateUrl: "./page-not-found.component.html",
+  styleUrls: ["./page-not-found.component.css"]
+})
+export class pagenotfoundComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
